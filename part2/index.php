df8aa1d4a5b89800ffcf7b7921d45d69a6a263f8
SouabeRaphael <R.souabe03@gmail.com>
2022-02-23 12:17:25 +0100

    initialisation part2

---
+++ part2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- appel dosier css -->
+    <link rel="stylesheet" href="../part2/assets/style.css">
+    <!-- appel dossier JavaScript -->
+    <script async src="../part2/assets/script.js"></script>
+</head>
+<body>
+    <main class="content-site">
+        <div class="wrapper-content">
+            <h1>allPIX</h1>
+            <form action="" method="POST" class="form">
+                <div class="login">
+                    <label class="label-form" for="login">Login</label>
+                    <input type="text" class="item-form" name="login">
+                </div>
+                <div class="password">
+                    <label class="label-form" for="password">Password</label>
+                    <input type="text" class="item-form" name="password">
+                </div>
+                <div class="btn-form">
+                    <button type="submit" class="btn">Connexion</button>
+                </div>
+            </form>
+        </div>
+    </main>
+    
+</body>
+</html>
